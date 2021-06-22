c9903a2e3983a74e8ffc21fb4873a687214c7587
Galymzhan Aigelov <aigelov@gmail.com>
2021-06-22 14:29:20 +0600

    sprint-1/step-1: Rename App.tsx to api.tsx

---
+++ src/components/api.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { AppHeader } from "./app-header/app-header";
+import { Main } from "./main/main";
+
+function Api() {
+  return (
+    <>
+      <AppHeader />
+      <Main />
+    </>
+  );
+}
+
+export default Api;
